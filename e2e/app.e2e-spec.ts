52cea3bae5769189735ea99c0030a09c7b04a17e
Rafael <rafaelbraga_22@hotmail.com>
2016-12-08 10:20:40 -0300

    Hello World app

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { Angularjs2FrontendPage } from './app.po';
+
+describe('angularjs2-frontend App', function() {
+  let page: Angularjs2FrontendPage;
+
+  beforeEach(() => {
+    page = new Angularjs2FrontendPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
