44e49fb6c1d95af332613d69e6ab6fe96123740c
Rafael <rafaelbraga_22@hotmail.com>
2017-01-16 23:23:21 -0300

    Usando lib interna e externa de javascript para testar o @types

--- src/app/modules/core/components/nav-bar/nav-bar.component.ts
+++ src/app/modules/core/components/nav-bar/nav-bar.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
 import { AuthenticationService } from './../../../shared/index';
 
+import * as lodash from 'lodash'; //npm install --save @types/lodash
+declare var jsSHA: any; //local library
+
 @Component({
   selector: 'pro-nav-bar',
   templateUrl: './nav-bar.component.html'
 })
 export class NavBarComponent {
 
-  constructor(private authService: AuthenticationService) { }
+  shaObj: any;
+  hash: String;
+
+  constructor(private authService: AuthenticationService) {
+    this.shaObj = new jsSHA("SHA-512", "TEXT");
+    this.shaObj.update("This is a test");
+    this.hash = this.shaObj.getHash("HEX");
+    console.log('local library - hash: ', this.hash);
+
+    var objA = {
+      "name": "colin"
+    }
+
+    var objB = lodash.cloneDeep(objA);
+    objB === objA // false
+
+    console.log('external library - lodash: ', 'objB: ' + objB.name + ", objA: " + objA.name);
+  }
+
 
   public logout(): void {
     this.authService.logout();
