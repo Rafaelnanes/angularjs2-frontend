452bf0378547cce283bb055619f0c34bc1fa5290
Rafael Nanes <rafaelbraga_22@hotmail.com>
2017-01-31 22:38:54 -0300

    apontando para o servidor do heroku

--- src/app/modules/shared/services/appSettings.ts
+++ src/app/modules/shared/services/appSettings.ts
@@ -1,5 +1,5 @@
 export class AppSettings {
-    public static BASE_URL: string = 'http://localhost:8080/java-backend-api/';
+    public static BASE_URL: string = 'https://java-backend.herokuapp.com/';
     public static CURRENT_USER: string = "currentUser";
     public static TOKEN_HEADER: string = "Authorization";
 }
