368b36994c93811181e0d40c04a52f4ecfdeb361
Rafael Nanes <rafaelbraga_22@hotmail.com>
2017-02-12 14:02:37 -0300

    Adicionado o capmo de data para produto

--- src/app/modules/product/components/product-main/product-main.component.ts
+++ src/app/modules/product/components/product-main/product-main.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit, ViewContainerRef } from '@angular/core';
+import { Component, OnInit, ViewContainerRef, EventEmitter } from '@angular/core';
 import { Router, Resolve, ActivatedRouteSnapshot, ActivatedRoute } from '@angular/router';
 import { FormGroup, FormControl, FormBuilder, Validators, Validator } from '@angular/forms';
+import { DatePipe } from '@angular/common';
 
 import { Product } from './../../models/index';
 import { ProductService, OperationEnum } from './../../index';
 import { DefaultHttp, GlobalService } from 'app/modules/shared/index';
 
 import { ToastsManager } from 'ng2-toastr/ng2-toastr';
+import * as lodash from 'lodash';
 
 @Component({
   selector: 'pro-product-main',
@@ -20,6 +22,8 @@ export class ProductMainComponent implements OnInit {
   public product: Product;
   public operation: OperationEnum;
   public productTypes: string[] = [];
+  public date: Date;
+  public isShowDatePicker: boolean;
 
   constructor(
     public fb: FormBuilder,
@@ -34,6 +38,8 @@ export class ProductMainComponent implements OnInit {
     this.buildForm();
     this.setFormModelValue();
     this.getAllProductTypes();
+    this.date = new Date();
+    this.watchDateChange();
   }
 
   private getAllProductTypes(): void {
@@ -50,6 +56,9 @@ export class ProductMainComponent implements OnInit {
       let id = this.route.snapshot.params['id'];
       this.productService.getById(id).then(response => {
         this.product = response.json();
+        if (this.product.date != null) {
+          this.product.date = this.formatDate(new Date(this.product.date));
+        }
         this.productForm.patchValue(this.product);
       });
 
@@ -73,7 +82,9 @@ export class ProductMainComponent implements OnInit {
   }
 
   private onSave(): void {
-    this.productService.save(this.productForm.value).then(response => {
+    let product: Product = this.productForm.value;
+    product.date = this.date.getTime().toString();
+    this.productService.save(product).then(response => {
       this.toastr.success('Product added');
       this.resetFormValues();
     }).catch(response => {
@@ -82,7 +93,9 @@ export class ProductMainComponent implements OnInit {
   }
 
   private onUpdate(): void {
-    this.productService.update(this.productForm.value).then(response => {
+    let product: Product = this.productForm.value;
+    product.date = this.date.getTime().toString();
+    this.productService.update(product).then(response => {
       this.toastr.success('Product updated');
       this.resetFormValues();
     }).catch(response => {
@@ -94,13 +107,32 @@ export class ProductMainComponent implements OnInit {
     this.isSubmitted = false;
   }
 
-  protected buildForm(): void {
+  public onChangeDate(event: Date): void {
+    this.date = event;
+    this.productForm.get('date').patchValue(this.formatDate(this.date));
+    this.isShowDatePicker = false;
+  }
+
+  private buildForm(): void {
     this.productForm = this.fb.group({
       name: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(20)]],
       value: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(20)]],
       productType: ['', Validators.required],
+      date: ['', Validators.required],
       id: ['']
     });
+  }
+
+  private watchDateChange(): void {
+    this.productForm.get('date').valueChanges.subscribe(data => {
+      let str: any = data;
+      str = str.split('/');
+      this.date = new Date(str[2], str[1] - 1, str[0]);
+    });
+  }
+
+  private formatDate(date: Date): string {
+    return new DatePipe('en-US').transform(date, 'dd/MM/yyyy');
   }
 
 }
--- src/app/modules/product/models/product.ts
+++ src/app/modules/product/models/product.ts
@@ -1,4 +1,4 @@
 export class Product {
-  constructor(public id?: number, public name?: string, public value?: number, public productType?: string) { }
+  constructor(public id?: number, public name?: string, public value?: number, public productType?: string, public date?: string) { }
 }
 
